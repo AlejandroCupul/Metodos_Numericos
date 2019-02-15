fcd52d337c332f56fc8ed8d9c301569488772433
Marco Alejandro Cupul Hau <marcoalejandro298@gmail.com>
2019-02-14 21:11:12 -0600

    Software que realiza cálculos de valor absoluto, relativo y porcentual, además de truncar y redondear cifras.

---
+++ Metodos_Numericos/src/metodos_numericos/Metodos_Numericos.java
@@ -0,0 +1,14 @@
+package metodos_numericos;
+
+public class Metodos_Numericos {
+    
+    Metodos_Numericos(){
+        Ventana v = new Ventana();
+        v.setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        Metodos_Numericos m = new Metodos_Numericos();
+    }
+    
+}
